f2194dbf3b46edcc37c2756afad364afb4effb8c
Thomas Brenna <tbre90@gmail.com>
2019-05-05 00:01:43 +0200

    Remove trait OutNames.

--- src/lib.rs
+++ src/lib.rs
@@ -22,76 +22,69 @@ impl From<std::io::Error> for FtaError {
     }
 }
 
-trait OutNames {
-    fn filename(&self) -> String;
-    fn header_guards(&self) -> Vec<String>;
-    fn arrayname(&self) -> String;
-    fn add_file_ending(&mut self, fend: String) -> Self;
+fn filename(s: String) -> String {
+    let mut name_reverse = String::new();
+
+    /* search for directory slashes
+     * only want filename when creating new header file
+     */
+    for c in s.chars().rev() {
+        if c == '/' || c == '\\' { 
+            break;
+        }
+        if c == '.' { // hit a file extension - don't want to include in header name
+            name_reverse.truncate(0);
+            continue;
+        }
+        name_reverse.push(c);
+    }
+
+    let mut name_proper = String::new();
+    for c in name_reverse.chars().rev() {
+        name_proper.push(c);
+    }
+
+    name_proper
 }
 
-impl OutNames for String {
-    fn filename(&self) -> String {
-        let mut name_reverse = String::new();
+fn header_guards(s: String) -> Vec<String> {
+    let mut ifndef = "#ifndef ".to_owned();
+    let name = s.clone().replace(".", "_").to_uppercase();
+    ifndef.push_str(&name);
 
-        /* search for directory slashes
-         * only want filename when creating new header file
-         */
-        for c in self.chars().rev() {
-            if c == '/' || c == '\\' { 
-                break;
-            }
-            if c == '.' { // hit a file extension - don't want to include in header name
-                name_reverse.truncate(0);
-                continue;
-            }
-            name_reverse.push(c);
-        }
+    let mut def = "#define ".to_owned();
+    def.push_str(&name);
 
-        let mut name_proper = String::new();
-        for c in name_reverse.chars().rev() {
-            name_proper.push(c);
-        }
+    let endif = "#endif".to_owned();
 
-        name_proper
-    }
+    let mut guard_vec: Vec<String> = vec!();
 
-    fn header_guards(&self) -> Vec<String> {
-        let mut ifndef = "#ifndef ".to_owned();
-        let name = self.clone().replace(".", "_").to_uppercase();
-        ifndef.push_str(&name);
+    guard_vec.push(ifndef);
+    guard_vec.push(def);
+    guard_vec.push(endif);
+    guard_vec
+}
 
-        let mut def = "#define ".to_owned();
-        def.push_str(&name);
+fn arrayname(s: String) -> String {
+    "arrayname".to_owned()
+}
 
-        let endif = "#endif".to_owned();
-
-        let mut guard_vec: Vec<String> = vec!();
-
-        guard_vec.push(ifndef);
-        guard_vec.push(def);
-        guard_vec.push(endif);
-        guard_vec
-    }
-    fn arrayname(&self) -> String {
-        "arrayname".to_owned()
-    }
-    fn add_file_ending(&mut self, fend: String) -> Self {
-        self.push_str(&fend);
-        self.to_string()
-    }
+fn add_file_ending(s: &mut String, fend: String) -> String {
+    s.push_str(".");
+    s.push_str(&fend);
+    s.to_string()
 }
 
 impl Converter {
     pub fn new(infile: String) -> Result<Converter, FtaError> {
         let inf = File::open(infile.clone())?;
 
-        let filename = infile.filename();
-        let header_guards = filename.clone().add_file_ending(".h".to_string()).header_guards();
+        let filename = filename(infile);
         Ok(Converter {
             in_file: inf,
-            out_filename: filename.clone().add_file_ending(".h".to_string()),
-            out_header_guards: header_guards,
-            out_array_name: filename.arrayname(),
+            out_filename: add_file_ending(&mut filename.clone(), "h".to_owned()),
+            out_header_guards: header_guards(add_file_ending(&mut filename.clone(), "h".to_owned())),
+            out_array_name: arrayname(filename),
         })
     }
 }
